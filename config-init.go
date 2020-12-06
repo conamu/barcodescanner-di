7711c8ed584914752ccd7aad79b0d33377697be4
conamu <constantin.amundsen@gmail.com>
2020-12-06 04:26:52 +0100

    created config base structs, added a dataInterface for all connector types with methods to read, write, edit and delete data. created codeSession struct to have an object which stores current data session, which will be wiped after the code is worked on.

---
+++ config-init.go
@@ -0,0 +1,77 @@
+package main
+
+import "github.com/spf13/viper"
+
+// Config Struct for the API
+type ApiConfig struct {
+	port string
+	enable bool
+}
+
+// Config Struct which will hold all configuration for the programm operation.
+type Config struct {
+	dbType string
+	dbPath string
+	sqlUser string
+	sqlPassword string
+	sqlDatabase string
+	sqlServer string
+	sqlPort string
+	api ApiConfig
+}
+
+func initConfig() *Config{
+	// Set configuration Defaults
+	viper.SetDefault("useFlatDB", true)
+	viper.SetDefault("useKeyValueDB", false)
+	viper.SetDefault("useMysqlDB", false)
+	viper.SetDefault("dbPath", "data/badger.bdb")
+	viper.SetDefault("flatPath", "data/database.csv")
+	viper.SetDefault("apiEndpointMode", false)
+	viper.SetDefault("apiEndpointPort", "8080")
+	viper.SetDefault("mysqlUser", "barcodeapp")
+	viper.SetDefault("mysqlPassword", "barcodeapp")
+	viper.SetDefault("mysqlDatabaseName", "barcodes")
+	viper.SetDefault("mysqlServerAddress", "localhost")
+	viper.SetDefault("mysqlServerPort", "3306")
+
+	// If it doesnt exist, create a new config file with the default values.
+	viper.SafeWriteConfigAs(".config.yaml")
+
+	// Read the config File
+	viper.SetConfigName(".config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	check(err)
+
+
+
+	// Create new Config object
+	var config = Config{}
+
+	// Set correct values depending on the selected Database type
+	if viper.GetBool("useFlatDB") {
+		config.dbType = "flat"
+		config.dbPath = viper.GetString("dbPath")
+	} else if viper.GetBool("useKeyValueDB") {
+		config.dbType = "kv"
+		config.dbPath = viper.GetString("flatPath")
+	} else if viper.GetBool("useMysqlDB") {
+		config.dbType = "sql"
+		config.sqlUser = viper.GetString("mysqlUser")
+		config.sqlPassword = viper.GetString("mysqlPassword")
+		config.sqlDatabase = viper.GetString("mysqlDatabaseName")
+		config.sqlServer = viper.GetString("mysqlServerAddress")
+		config.sqlPort = viper.GetString("mysqlPort")
+	}
+
+	// Set Api Configuration if API Mode is on
+	if viper.GetBool("apiEndpointMode") {
+		config.api.enable = true
+		config.api.port = viper.GetString("apiEndpointPort")
+	}
+
+	return &config
+
+}
--- main.go
+++ main.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"log"
+	"os"
+)
+
+type dataInterface interface {
+	getData() (bool,error , []string)
+	writeData() (bool, error)
+	editData() (bool, error)
+	deleteData() (bool, error)
+}
+
+type codeSession struct {
+	dataInterface *dataInterface
+	code string
+	codeValid bool
+	data []string
+}
+
+func newCodeSession(dataInterface *dataInterface) codeSession  {
+	return codeSession {
+		dataInterface,
+		"",
+		false,
+		nil,
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+var config = initConfig()
+
+func main() {
+	// Check if we have the data/ folder, if not, check if we need it, if yes, create it.
+	if _, err := os.Stat("data"); os.IsNotExist(err) {
+		if config.dbType != "sql" {
+			os.Mkdir("data", 0755)
+		}
+	}
+
+	// Create a new CodeSession based on configured db Type
+	var session codeSession
+	switch config.dbType {
+	case "flat":
+		session = newCodeSession(flatConnector)
+	case "kv":
+		session = newCodeSession(kvConnector)
+	case "sql":
+		session = newCodeSession(sqlConnector)
+	}
+
+
+}
